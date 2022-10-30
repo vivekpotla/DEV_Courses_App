eabbe71aec077ed348bd428ddb488af4fc35d2ec
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2022-10-30 14:42:28 +0530

    completed app

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,14 +6,17 @@ export default function Profile() {
 
     const userType = localStorage.getItem("userType");
     const userId  = localStorage.getItem("userId");
-    const [Data,setData] = useState(null);
+    const [Data,setData] = useState({});
      
     
-    useEffect(()=>
-    {
-       axios.get(`http://localhost:5003/api/${userType}/byid/${userId}`,).catch((err)=>console.log(err)).then((res)=>setData(res.data.user));
-
-    },[])
+    
+     const data =  async()=>
+      {
+        await axios.get(`http://localhost:5003/api/${userType}/byid/${userId}`,).catch((err)=>console.log(err)).then((res)=>setData(res.data.user));
+      }
+    data();
+      
+   
   
   console.log(Data);   
  
@@ -28,7 +31,7 @@ export default function Profile() {
                   style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
                   <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
                     alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
-                  <MDBTypography tag="h5">He</MDBTypography>
+                  <MDBTypography tag="h5">{Data.name}</MDBTypography>
                   <MDBCardText>Web Designer</MDBCardText>
                   <MDBIcon far icon="edit mb-5" />
                 </MDBCol>
@@ -39,11 +42,11 @@ export default function Profile() {
                     <MDBRow className="pt-1">
                       <MDBCol size="12" className="mb-3">
                         <MDBTypography tag="h6">Email</MDBTypography>
-                        <MDBCardText className="text-muted"></MDBCardText>
+                        <MDBCardText className="text-muted">{Data.email}</MDBCardText>
                       </MDBCol>
                       <MDBCol size="12" className="mb-3">
                         <MDBTypography tag="h6">Phone</MDBTypography>
-                        <MDBCardText className="text-muted"></MDBCardText>
+                        <MDBCardText className="text-muted">{Data.mobileno}</MDBCardText>
                       </MDBCol>
                     </MDBRow>
 
@@ -54,11 +57,11 @@ export default function Profile() {
                         <MDBTypography tag="h6">
                           {userType=="instructor"? "Total Courses Created":"Total Courses Enrolled"}
                         </MDBTypography>
-                        <MDBCardText className="text-muted"></MDBCardText>
+                        {/* <MDBCardText className="text-muted">   {userType=="instructor"? Data.createdCourses.length : 12}</MDBCardText> */}
                       </MDBCol>
                       <MDBCol size="6" className="mb-3">
                         <MDBTypography tag="h6">Phone</MDBTypography>
-                        <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                        <MDBCardText className="text-muted">{Data.mobileno}</MDBCardText>
                       </MDBCol>
                     </MDBRow>
 
